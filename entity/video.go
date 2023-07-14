9b4af46f6b3eb95c41de641e0d5e5610dbe5246a
Bcpeh <pehboonchong@gmail.com>
2023-07-14 17:25:48 +0800

    added html, templates, multi route groupings

--- entity/video.go
+++ entity/video.go
@@ -1,8 +1,8 @@
 package entity
 
 type Video struct {
-	Title       string `json:"title" binding:"min=2,max=10" validate:"is-cool"`
-	Description string `json:"description" binding:"max=20"`
+	Title       string `json:"title" binding:"min=2,max=200" validate:"is-cool"`
+	Description string `json:"description" binding:"max=200"`
 	Url         string `json:"url" binding:"required,url"`
 	Author      Person `json:"author" binding:"required"`
 }
